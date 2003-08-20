64bc242cd957b295483cf3a99f790430a75f305a
Aron Birkir Gudmundsson <aron@idega.com>
2003-08-20 17:33:20 +0000

    *** empty log message ***

---
+++ src/java/se/idega/idegaweb/commune/accounting/userinfo/presentation/BruttoIncomeWindow.java
@@ -0,0 +1,38 @@
+/*
+ * Created on Aug 18, 2003
+ *
+ */
+package se.idega.idegaweb.commune.accounting.userinfo.presentation;
+
+import com.idega.presentation.IWContext;
+import com.idega.presentation.ui.Window;
+
+/**
+ * BruttoIncomeWindow
+ * @author aron 
+ * @version 1.0
+ */
+
+public class BruttoIncomeWindow extends Window {
+
+	public BruttoIncomeWindow() {
+		this.setWidth(500);
+		this.setHeight(500);
+		this.setScrollbar(true);
+		this.setResizable(true);
+		this.setAllMargins(0);
+	}
+	
+	/**
+	 * @see com.idega.presentation.PresentationObject#main(IWContext)
+	 */
+	public void main(IWContext iwc) throws Exception {
+	  BruttoIncomeEditor editor = new BruttoIncomeEditor();
+	  //editor.setShowCloseButton(true);
+	  add(editor);
+	}
+	
+	public static String getUserIDParameterName(){
+		return BruttoIncomeEditor.PRM_USER_ID;
+	}
+}
