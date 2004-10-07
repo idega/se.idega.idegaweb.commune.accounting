28321938eb058e09f762a3f57dba5f389f4a2057
Thomas Hilbig <thomas@idega.com>
2004-10-07 15:23:39 +0000

    several classes of the package se.idega.idegaweb.commune.accounting.resource were moved to se.idega.idegaweb.commune.extra

---
+++ src/java/se/idega/idegaweb/commune/accounting/extra/business/ResourceBusinessHome.java
@@ -0,0 +1,8 @@
+package se.idega.idegaweb.commune.accounting.extra.business;
+
+
+public interface ResourceBusinessHome extends com.idega.business.IBOHome
+{
+ public ResourceBusiness create() throws javax.ejb.CreateException, java.rmi.RemoteException;
+
+}
