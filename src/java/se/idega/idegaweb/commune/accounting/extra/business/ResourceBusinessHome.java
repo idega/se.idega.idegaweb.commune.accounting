68a0c813264afb2d798b8ae126dad4601223e968
Thomas Hilbig <thomas@idega.com>
2004-10-14 14:54:15 +0000

    move classes from accounting bundle down to care bundle

--- src/java/se/idega/idegaweb/commune/accounting/extra/business/ResourceBusinessHome.java
+++
@@ -1,8 +0,0 @@
-package se.idega.idegaweb.commune.accounting.extra.business;
-
-
-public interface ResourceBusinessHome extends com.idega.business.IBOHome
-{
- public ResourceBusiness create() throws javax.ejb.CreateException, java.rmi.RemoteException;
-
-}
