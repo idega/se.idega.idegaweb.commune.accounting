64ddac04b8d44920d47a1c5dea62aff76eed2e9c
Thomas Hilbig <thomas@idega.com>
2004-10-15 10:19:46 +0000

    moved AccountingSession down to care

--- src/java/se/idega/idegaweb/commune/accounting/business/AccountingSessionHome.java
+++
@@ -1,8 +0,0 @@
-package se.idega.idegaweb.commune.accounting.business;
-
-
-public interface AccountingSessionHome extends com.idega.business.IBOHome
-{
- public AccountingSession create() throws javax.ejb.CreateException, java.rmi.RemoteException;
-
-}
