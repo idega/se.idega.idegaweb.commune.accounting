cb92bcffde454c061930b490e78bee08224b47d2
Thomas Hilbig <thomas@idega.com>
2004-10-15 10:36:38 +0000

    ProviderAccountingProperties and ProviderType moved down to care

--- src/java/se/idega/idegaweb/commune/accounting/school/data/ProviderAccountingPropertiesHome.java
+++
@@ -1,9 +0,0 @@
-package se.idega.idegaweb.commune.accounting.school.data;
-
-
-public interface ProviderAccountingPropertiesHome extends com.idega.data.IDOHome
-{
- public ProviderAccountingProperties create() throws javax.ejb.CreateException;
- public ProviderAccountingProperties findByPrimaryKey(Object pk) throws javax.ejb.FinderException;
-	public java.util.Collection findAllIdsByPaymentByInvoice (boolean p0)throws javax.ejb.FinderException;
-}
